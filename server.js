9eb79cf3536054480e6b6a94cf35809135bbd8eb
hagenwatkins <hd_watkins@yahoo.com>
2017-08-17 10:14:26 -0500

    env

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const app = express();
 
 app.use(express.static('public'));
 
-
+console.log('==================');
+console.log(process.env);
 app.listen(7990, () => {
   console.log('Listening on port 7990');
 
