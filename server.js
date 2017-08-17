5e26ad8061b6b286ec1f4e83d9dc139f3fc066a8
hagenwatkins <hd_watkins@yahoo.com>
2017-08-17 10:18:20 -0500

    env again

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static('public'));
 
 console.log('==================');
 console.log(process.env);
-app.listen(7990, () => {
-  console.log('Listening on port 7990');
+app.listen(process.env.Port, () => {
+  console.log('Listening on port 7990', ${process.env.PORT});
 
 });
