621cd47b2154b07167d1d35f51c79b724b2d02c5
hagenwatkins <hd_watkins@yahoo.com>
2017-08-17 09:52:31 -0500

    initial

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+
+const app = express();
+
+
+
+app.use(express.static('public'));
+
+
+app.listen(7990, () => {
+  console.log('Listening on port 7990');
+
+});
